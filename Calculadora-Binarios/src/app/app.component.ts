443746ff6b48996723db9a550ddb061fcd5de409
BeatrizFabri <bia.fabri@outlook.com>
2021-10-07 21:11:21 -0300

    layout inicial

---
+++ Calculadora-Binarios/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css', '../../node_modules/bootstrap/dist/css/bootstrap.css'],
+})
+export class AppComponent {
+  title = 'Calculadora-Binarios';
+  userForm = new FormGroup({
+    numero: new FormControl(''),
+  });
+
+  calcular() {
+    console.log(this.userForm.get('numero'));
+  }
+}
